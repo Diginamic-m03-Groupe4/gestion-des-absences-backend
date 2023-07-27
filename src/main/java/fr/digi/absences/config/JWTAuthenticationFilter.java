9183cee179a8de3affa13f20413c938b5c6c45a9
Abel Ciccoli <abelcoli@outlook.fr>
2023-07-27 14:45:02 +0200

    feat(security): ajout des fichiers de sécurité

---
+++ src/main/java/fr/digi/absences/config/JWTAuthenticationFilter.java
@@ -0,0 +1,60 @@
+package fr.digi.absences.config;
+
+import fr.digi.absences.service.JwtService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RequiredArgsConstructor
+@Configuration
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws ServletException, IOException {
+        if (req.getCookies() != null) {
+            Stream.of(req.getCookies())
+                    .filter(cookie -> cookie.getName().equals(jwtService.getCookie()))
+                    .map(Cookie::getValue)
+                    .forEach(token -> {
+                        Claims body = Jwts
+                                .parserBuilder()
+                                .setSigningKey(jwtService.getSecretKey())
+                                .build()
+                                .parseClaimsJws(token)
+                                .getBody();
+
+                        String username = body.getSubject();
+
+
+                        List<LinkedHashMap<String, String>> roles = body.get("roles", List.class);
+
+                        logger.info(roles);
+                        List<SimpleGrantedAuthority> authorities = roles
+                                .stream()
+                                .map(map -> new SimpleGrantedAuthority(map.get("role")))
+                                .collect(Collectors.toList());
+                        Authentication authentication = new UsernamePasswordAuthenticationToken(username, null, authorities);
+                        SecurityContextHolder.getContext().setAuthentication(authentication);
+                    });
+        }
+        chain.doFilter(req, res);
+    }
+}
--- src/main/java/fr/digi/absences/entity/Roles.java
+++ src/main/java/fr/digi/absences/entity/Roles.java
@@ -1,5 +1,16 @@
 package fr.digi.absences.entity;
 
+import lombok.AllArgsConstructor;
+
 public enum Roles {
-    EMPLOYEE, MANAGER, ADMINISTRATEUR
+    EMPLOYEE("EMPLOYEE"), MANAGER("MANAGER"), ADMINISTRATEUR("ADMIN");
+    private String role;
+
+    Roles(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
 }
