55fdf391488e0c8af0e39643c997e60c8a3da5d5
arny-wilk <arnywilk@gmail.com>
2023-07-27 14:59:00 +0200

    package repository EmployeeRepo

--- src/main/java/fr/digi/absences/repository/EmployeeRepo.java
+++ src/main/java/fr/digi/absences/repository/EmployeeRepo.java
@@ -1,4 +1,9 @@
 package fr.digi.absences.repository;
 
-public class EmployeeRepo {
+import fr.digi.absences.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
 }
