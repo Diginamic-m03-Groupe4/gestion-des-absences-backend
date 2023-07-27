938d5fcc178d36437e7f2a77b93e803a0bb236f5
Abel Ciccoli <abelcoli@outlook.fr>
2023-07-27 13:09:22 +0200

    feat(employee): ajout du début de la sécu

--- src/main/java/fr/digi/absences/entity/Employee.java
+++ src/main/java/fr/digi/absences/entity/Employee.java
@@ -1,4 +1,26 @@
 package fr.digi.absences.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String nom;
+    private String prenom;
+    private String password;
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<Roles> roles = new ArrayList<>();
+
 }
---
+++ src/main/java/fr/digi/absences/entity/Roles.java
@@ -0,0 +1,5 @@
+package fr.digi.absences.entity;
+
+public enum Roles {
+    EMPLOYEE, MANAGER, ADMINISTRATEUR
+}
