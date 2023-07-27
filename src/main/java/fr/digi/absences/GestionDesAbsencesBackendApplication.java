8dcd9e3a55acec71bcb043b5bd26b089101551ef
Goertz Juliette <129963661+JulietteGTB@users.noreply.github.com>
2023-07-27 12:01:54 +0200

    init

---
+++ src/main/java/fr/digi/absences/GestionDesAbsencesBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.digi.absences;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDesAbsencesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionDesAbsencesBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/digi/absences/GestionDesAbsencesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.digi.absences;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDesAbsencesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
