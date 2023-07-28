7418772a5bb550039e125edcb7363f60f8dc0ff1
Abel Ciccoli <abelcoli@outlook.fr>
2023-07-28 15:32:23 +0200

    feat(exceptionHanlder): Ajout d'un exceptionhandler renvoyant des requêtes au status 400

--- src/main/java/fr/digi/absences/controller/CongePayeCtrl.java
+++ src/main/java/fr/digi/absences/controller/CongePayeCtrl.java
@@ -5,8 +5,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.Locale;
+
 @Controller
-@RequestMapping("conges")
+@RequestMapping("api/v1/conges")
 public class CongePayeCtrl {
 
     @DeleteMapping
