553ae996cecb16e7b5998f79137b1ad45fc0f34a
arny-wilk <arnywilk@gmail.com>
2023-08-18 11:10:06 +0200

    feat(RTTEmployeurCtrl): No change

--- src/test/java/fr/digi/absences/repository/AbsenceRepoTest.java
+++ src/test/java/fr/digi/absences/repository/AbsenceRepoTest.java
@@ -1,12 +1,16 @@
 package fr.digi.absences.repository;
 
+import fr.digi.absences.GestionDesAbsencesBackendApplication;
 import fr.digi.absences.consts.Roles;
 import fr.digi.absences.consts.StatutAbsence;
 import fr.digi.absences.consts.TypeConge;
 import fr.digi.absences.entity.Absence;
 import fr.digi.absences.entity.Departement;
 import fr.digi.absences.entity.Employee;
+import fr.digi.absences.service.AbsenceSrvc;
 import fr.digi.absences.service.JwtService;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.ClassRule;
@@ -16,8 +20,11 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -41,6 +48,7 @@ import org.testcontainers.utility.DockerImageName;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.mockito.Mockito.doReturn;
@@ -56,7 +64,6 @@ import static org.mockito.BDDMockito.*;
 => rollback à la fin de chaque méthode
 */
 
-//@SpringBootTest
 @Testcontainers
 @Slf4j
 @WebMvcTest(ConfigurableApplicationContext.class)
@@ -72,14 +79,13 @@ class AbsenceRepoTest {
         registry.add("spring.datasource.password", container::getPassword);
     }
 
-
     @BeforeAll
     static void startContainer() {
         container.start();
     }
 
-    @Mock
-    Absence absence;
+    @MockBean
+    TestEntityManager tem;
 
     @MockBean
     AbsenceRepo absenceRepo;
@@ -88,10 +94,13 @@ class AbsenceRepoTest {
     Employee employee;
 
     @MockBean
-    EmployeeRepo employeeRepo;
+    Departement departement;
 
     @MockBean
-    Departement departement;
+    Absence absence;
+
+    @MockBean
+    EmployeeRepo employeeRepo;
 
     @MockBean
     DepartementRepo departementRepo;
@@ -102,6 +111,36 @@ class AbsenceRepoTest {
 
     @BeforeEach
     void setUp() {
+        LocalDate dateDebut = LocalDate.of(2023, 7, 17);
+        LocalDate dateFin = LocalDate.of(2023, 7, 25);
+
+        this.employee = new Employee();
+        this.employee.setId(1L);
+        this.employee.setNom("Toto");
+        this.employee.setPrenom("Latete");
+        this.employee.setEmail("toto@gmail.com");
+        this.employee.setRole(Roles.EMPLOYEE);
+        this.employee.setAbsences(new ArrayList<>());
+        this.employee.setAbsenceRejetees(new ArrayList<>());
+        List<Absence> absences = this.employee.getAbsences();
+
+        this.departement = new Departement();
+        this.departement.setId(1L);
+        this.departement.setLibelle("Hérault 34");
+        this.departement.setEmployees(new ArrayList<>());
+        this.departement.getEmployees().add(this.employee);
+
+        this.absence = new Absence();
+        this.absence.setId(1L);
+        this.absence.setDateDebut(dateDebut);
+        this.absence.setDateFin(dateFin);
+        this.absence.setMotif("Congé Juilletiste");
+        this.absence.setEmployee(employee);
+        this.absence.setTypeConge(TypeConge.SANS_SOLDE);
+        this.absence.setStatus(StatutAbsence.ATTENTE_VALIDATION);
+
+        absences.add(this.absence);
+
     }
 
 
@@ -111,10 +150,16 @@ class AbsenceRepoTest {
     @Test
     void getAbsenceRepoTest() {
         // Hypothese
+        when(this.absenceRepo.getReferenceById(1L)).thenReturn(this.absence);
 
         // Execution code
+        Optional<Absence> byId = absenceRepo.findById(1L);
+
+        // Gestion de l'exception en arrière plan
+        doThrow(EntityNotFoundException.class).when(this.absenceRepo).getReferenceById(1L);
 
         // Vérification du résultat
+        Assertions.assertThat(byId).contains(this.absence);
 
     }
 
@@ -125,6 +170,7 @@ class AbsenceRepoTest {
     void createAbsenceRepoTest() {
 
         // mise en place pour le test
+
 
         // Invocation de la méthode
 
