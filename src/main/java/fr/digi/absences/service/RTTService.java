3f5d56ac13e5bd49c871f41c6417e9742805eaa3
arny-wilk <arnywilk@gmail.com>
2023-08-21 15:14:52 +0200

    feature(Merge_V1): Business Logic RTT employeur done

--- src/main/java/fr/digi/absences/service/RTTService.java
+++ src/main/java/fr/digi/absences/service/RTTService.java
@@ -1,10 +1,15 @@
 package fr.digi.absences.service;
 
+import fr.digi.absences.consts.StatutAbsence;
 import fr.digi.absences.consts.StatutAbsenceEmployeur;
+import fr.digi.absences.dto.AbsenceDto;
 import fr.digi.absences.dto.RTTEmployeurDTO;
+import fr.digi.absences.entity.Absence;
 import fr.digi.absences.entity.RTTEmployeur;
 import fr.digi.absences.exception.BrokenRuleException;
+import fr.digi.absences.mapper.AbsenceMap;
 import fr.digi.absences.mapper.RTTEmployeurMap;
+import fr.digi.absences.repository.AbsenceRepo;
 import fr.digi.absences.repository.EmployeeRepo;
 import fr.digi.absences.repository.RTTEmployeurRepo;
 import fr.digi.absences.utils.DateUtils;
@@ -13,9 +18,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
+import java.util.*;
 
 @Service
 @AllArgsConstructor
@@ -29,51 +32,54 @@ public class RTTService {
 
     private JourFeriesService jourFeriesService;
 
+    private AbsenceSrvc absenceSrvc;
+    private AbsenceRepo absenceRepo;
+    private AbsenceMap absenceMap;
+
 
     private int getNbRttRestants() {
         return 0;
     }
 
     public Collection<RTTEmployeur> getRTTEmployeur() {
-
-        Collection<RTTEmployeur> byDateAndStatutAbsenceEmployeur = rttEmployeurRepo.findAll();
-
-        return byDateAndStatutAbsenceEmployeur;
+        return rttEmployeurRepo.findAll();
     }
 
-    public RTTEmployeurDTO getRTTEmployeurByID(Long id){
-
+    public RTTEmployeurDTO getRTTEmployeurByID(Long id) {
         RTTEmployeur rttEmployeur = this.rttEmployeurRepo.findById(id).orElseThrow(EntityExistsException::new);
-
         return rttEmployeurMap.toRTTEmployeurDTO(rttEmployeur);
     }
 
     public RTTEmployeur createRTT(RTTEmployeurDTO rttEmployeurDTO) {
         applyCreationLogic(rttEmployeurDTO);
 
+        List<Absence> absenceMatchRttEmployeur = absenceRepo.findAbsenceMatchRttEmployeur(rttEmployeurDTO.getDate());
+        if(!absenceMatchRttEmployeur.isEmpty()){
+            Iterator<Absence> iter = absenceMatchRttEmployeur.listIterator();
+            while (iter.hasNext()) {
+                Absence absence = iter.next();
+                absence.setStatus(StatutAbsence.REJETEE);
+                absenceRepo.save(absence);
+            }
+        }
+
         RTTEmployeur rttEmployeur = rttEmployeurMap.toRTTEmployeur(rttEmployeurDTO);
-
         return this.rttEmployeurRepo.save(rttEmployeur);
-
     }
 
-    public void updateRTT(RTTEmployeurDTO rttEmployeurDTO, Long id){
-
+    public void updateRTT(RTTEmployeurDTO rttEmployeurDTO, Long id) {
         applyCreationLogic(rttEmployeurDTO);
-
         RTTEmployeur rttEmployeur = rttEmployeurRepo.findById(id).orElseThrow(EntityExistsException::new);
         RTTEmployeur rttEmployeurUpdated = rttEmployeurMap.toRTTEmployeur(rttEmployeurDTO);
 
-        if(rttEmployeur.equals(rttEmployeurUpdated)){
+        if (rttEmployeur.equals(rttEmployeurUpdated)) {
             throw new BrokenRuleException("Aucune mise à jour à faire !");
         }
-
         rttEmployeur = rttEmployeurUpdated;
         this.rttEmployeurRepo.save(rttEmployeur);
-
     }
 
-    public void deleteRTT(Long id){
+    public void deleteRTT(Long id) {
         RTTEmployeur rttEmployeur = this.rttEmployeurRepo.findById(id).orElseThrow(EntityExistsException::new);
         rttEmployeurRepo.delete(rttEmployeur);
     }
@@ -81,8 +87,8 @@ public class RTTService {
     private void applyCreationLogic(RTTEmployeurDTO rttEmployeurDTO) {
 
         // TODO LOGIC METIER: Le statut de l'absence doit etre en INITIALE
-        if(!rttEmployeurDTO.getStatutAbsenceEmployeur().equals(StatutAbsenceEmployeur.INITIALE)){
-            throw new BrokenRuleException("Le statut de l'absence doit etre initiale");
+        if (rttEmployeurDTO.getStatutAbsenceEmployeur().equals(StatutAbsenceEmployeur.VALIDEE)) {
+            throw new BrokenRuleException("On ne peut pas modifier ou creer une journée de RTT employeur déjà validée");
         }
 
         // TODO LOGIC METIER: un RTT employeur doit etre dans le future
@@ -91,8 +97,12 @@ public class RTTService {
         }
 
         // TODO LOGIC METIER; un RTT employeur ne peut pas etre déclaré sur un jour férié
-        if (DateUtils.isRTTOnJourFerie(rttEmployeurDTO.getDate() ,jourFeriesService.joursFeries(rttEmployeurDTO.getDate().getYear()))) {
+        if (DateUtils.isRTTOnJourFerie(rttEmployeurDTO.getDate(), jourFeriesService.joursFeries(rttEmployeurDTO.getDate().getYear()))) {
             throw new BrokenRuleException("Le jour RTT posé ne peut pas être un jour férié.");
+        }
+
+        if(DateUtils.isBusinessDay(rttEmployeurDTO.getDate())){
+            throw new BrokenRuleException("Il est interdit de saisir une RTT employeur un samedi ou un dimanche");
         }
 
         // TODO LOGIC METIER: Un RTT employeur ne peut pas etre déclaré sur un jour RTT existant
@@ -103,9 +113,10 @@ public class RTTService {
     }
 
     /**
-     *********************** JUST IN CASE ****************************
+     * ********************** JUST IN CASE ****************************
      * Cette méthode permet de verifier que le nom de l'employeur
      * existe bien en base de données
+     *
      * @param employeurName
      */
     private boolean verifyIfEmployeurNameExist(String employeurName) {
