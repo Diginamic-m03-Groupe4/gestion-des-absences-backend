3c1204edb9a345335e1f9ba2457f8aadae97b5f9
Abel Ciccoli <abelcoli@outlook.fr>
2023-08-22 17:03:20 +0200

    fix(merge): merged the branch into the other branch

---
+++ src/test/java/fr/digi/absences/controller/JoursFerieCtrlTest.java
@@ -0,0 +1,16 @@
+package fr.digi.absences.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JoursFerieCtrlTest {
+
+    @Test
+    void getJoursFeries() {
+    }
+
+    @Test
+    void changeJourFerieIsTravaille() {
+    }
+}
---
+++ src/test/java/fr/digi/absences/service/TraitementNuitTest.java
@@ -0,0 +1,12 @@
+package fr.digi.absences.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TraitementNuitTest {
+
+    @Test
+    void executerTraitementNuit() {
+    }
+}
